cfe895856376a42dcc83d889ae9733a4b2678580
Gabriela Trevisan <gabitrevis@gmail.com>
2025-09-16 15:53:54 -0300

    :hammer: versao 2.0

--- CarteiraCerta.Model/Carteira.cs
+++ CarteiraCerta.Model/Carteira.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 namespace CarteiraCerta.Model
 {
     public class Carteira
--- CarteiraCerta.Model/Usuario.cs
+++ CarteiraCerta.Model/Usuario.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarteiraCerta.Model
 {
     public class Usuario
